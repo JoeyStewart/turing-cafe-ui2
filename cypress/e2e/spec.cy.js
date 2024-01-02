808396c76e9d1f59d8aeae513e05700a2163459e
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-02 14:01:42 -0500

    Add tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,22 @@
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:3000/')
   })
+
+  it('should recognize and interact with form inputs and make a reservation', () => {
+  cy.get('input[name="name"]').type('Joey Tomato').should('have.value', 'Joey Tomato');
+  cy.get('input[name="date"]').type('3/11/2122').should('have.value', '3/11/2122');
+  cy.get('input[name="time"]').type('12:00').should('have.value', '12:00');
+  cy.get('input[name="guests"]').type('5').should('have.value', '5');
+  cy.get('button').contains('Make Reservation').click();
+  })
+
+  it('should delete reservation when cancel is clicked', () => {
+    cy.get('.card').should('have.length.greaterThan', 0);
+    cy.get('.card button').click(); 
+    cy.window().its('deleteRes').should('delete')
+    cy.get('.card').should('not.exist');
+    });
+  
 })
+
