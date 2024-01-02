9823ba3fa435ab671354196d3801537279df7cf8
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-02 12:42:55 -0500

    Elements placed on card

--- src/Reservation/Reservation.js
+++ src/Reservation/Reservation.js
@@ -2,16 +2,16 @@ import Card from '../Card/Card.js';
 import './Reservation.css';
 
 
-function Reservation({reserv}){
+function Reservation({reservations}){
 
 
-const ResCards = reserv.map(res => {
+const ResCards = reservations.map(res => {
    return (
         <Card
         name={res.name}
         description={res.date}
         time={res.time}
-        guests={'Number of guests: ' + res.guests} 
+        guests={'Number of guests: ' + res.number} 
         id={res.id}
         key={res.id}
   />
