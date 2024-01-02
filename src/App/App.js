bc558cdc903f017bbedbca954199f83021ce06c7
Joseph Stewart <JosephStewart1752@gmail.com>
2024-01-02 12:04:21 -0500

    Basic card layout

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,52 @@
 import './App.css';
+import Reservation from '../Reservation/Reservation.js';
+import Form from '../Form/Form.js';
+import { useState } from 'react';
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className='app-title'>Turing Cafe Reservations</h1>
-      <div className='resy-form'>
-      </div>
-      <div className='resy-container'>
-      </div>
-    </div>
-  );
-}
+function App(){
 
-export default App; 
+  const [reservations, setReservations] = useState([])
+   
+  function addRes (newRes) {
+    setReservations([...reservations, newRes])
+  }
+  
+  function deleteRes(id){
+    console.log(id);
+    const badRes = reservations.filter(res => res.id !== id)
+    setReservations(badRes)
+  }
+ 
+ 
+  return(
+    <main className='App'>
+        <h1 className='app-title'>Turing Cafe Reservations</h1>
+        <div className='resy-form'>
+          <Form addRes={addRes}/>
+        </div>
+        <div className='resy-container'>
+          <Reservation reservation={reservations} deleteRes={deleteRes}/>
+        </div>
+    </main>
+  )
+ }
+ 
+ 
+ export default App;
+
+ // function App() {
+//   return (
+//     <div className="App">
+//       <h1 className='app-title'>Turing Cafe Reservations</h1>
+//       <div className='resy-form'>
+//       </div>
+//       <div className='resy-container'>
+//       </div>
+{/* <Form addIdea={addIdea}/>
+<Ideas ideas={ideas} deleteIdea={deleteIdea}/> */}
+//     </div>
+//   );
+// }
+
+// export default App; 
